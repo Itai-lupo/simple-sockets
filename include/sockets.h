a21ef1367cbf48ede606aaba856ef451461d0824
itai <itailupo@gmail.com>
2023-12-14 03:20:25 +0200

    docstring and set errno to 0 when used

--- include/sockets.h
+++ include/sockets.h
@@ -1,22 +1,57 @@
 #pragma once
 
-#include "files.h"
+#include "types/err_t.h"
+#include "types/fd_t.h"
 
-#include "defines/errType.h"
 #include <sys/socket.h>
 
 #ifdef __cplusplus
 extern "C"
 {
 #endif
+	/**
+	 * @brief
+	 *
+	 * @param sd the socket descriptor to send to
+	 * @param buf the buffer to send
+	 * @param size the size of the buffer
+	 * @param flags the flags to send with
+	 * @param destAddr the address to send t o
+	 * @param addrlen the address length
+	 * @param bytesWritten how many bytes were were send, should be the same as size on full send
+	 * @return err_t err_t if there was an error in sendto or in one of the parms
+	 * @see https://man7.org/linux/man-pages/man2/sendto.2.html
+	 *
+	 */
+	THROWS err_t safeSendto(fd_t sd, const void *const buf, size_t size, int flags, const struct sockaddr *destAddr,
+							socklen_t addrlen, ssize_t *bytesWritten);
 
-	THROWS err_t safe_sendto(fd_t sd, const void *const buf, size_t size, int flags, const struct sockaddr *destAddr,
-							 socklen_t addrlen, ssize_t *bytesWritten);
+	/**
+	 * @brief create a new socket descriptor
+	 *
+	 * @param domain the protocol family which will be used for communication.
+	 * @param type the communication semantics and flags to modify the behavior of the socket.
+	 * @param protocal a particular protocol to be used with the socket, can be 0 if there is only one option.
+	 * @param sd the resulted socket descriptor
+	 * @return err_t if there was an error while creating the socket or in one of the parms
+	 *
+	 * @see https://man7.org/linux/man-pages/man2/socket.2.html
+	 * @see https://man7.org/linux/man-pages/man7/address_families.7.html
+	 * @see https://man7.org/linux/man-pages/man5/protocols.5.html
+	 */
+	THROWS err_t createSocket(int domain, int type, int protocal, fd_t *sd);
 
-	THROWS err_t create_socket(int domain, int type, int protocal, fd_t *sd);
-
-	THROWS err_t safe_bind(fd_t sd, const struct sockaddr *const addr, socklen_t addrlen);
-
+	/**
+	 * @brief bind sd to addr
+	 *
+	 * @param sd the file descriptor of the socket to bind
+	 * @param addr the address to bind to
+	 * @param addrlen the length of the address
+	 * @return err_t if there was an error while binding or in one of the parms
+	 *
+	 * @see https://man7.org/linux/man-pages/man2/bind.2.html
+	 */
+	THROWS err_t safeBind(fd_t sd, const struct sockaddr *const addr, socklen_t addrlen);
 
 #ifdef __cplusplus
 }
--- src/sockets.c
+++ src/sockets.c
@@ -1,49 +1,66 @@
 #include "sockets.h"
 
-
 #include "defaultTrace.h"
 #include "err.h"
 #include <sys/socket.h>
 
-THROWS  err_t safe_sendto(fd_t sd, const void *const buf, size_t size, int flags, const struct sockaddr *destAddr,
-								socklen_t addrlen, ssize_t *bytesWritten)
+/**
+ * @brief how many times the io opreations will retry to handle EINTR or EAGAIN
+ * @see safeSendto
+ */
+#define NAX_IO_RETRIES 3
+
+THROWS err_t safeSendto(fd_t sd, const void *const buf, size_t size, int flags, const struct sockaddr *destAddr,
+						socklen_t addrlen, ssize_t *bytesWritten)
 {
 	err_t err = NO_ERRORCODE;
-	ssize_t tempBytesWritten = 0;
+	ssize_t tempBytesWritten = -1;
+	errno = 0;
 
 	QUITE_CHECK(IS_VALID_FD(sd));
 	QUITE_CHECK(destAddr != NULL);
 	QUITE_CHECK(bytesWritten != NULL);
 	QUITE_CHECK(buf != NULL);
 
-	tempBytesWritten = sendto(sd.fd, buf, size, flags, destAddr, addrlen);
+	for (size_t i = 0; i < NAX_IO_RETRIES && tempBytesWritten == -1; i++)
+	{
+		tempBytesWritten = sendto(sd.fd, buf, size, flags, destAddr, addrlen);
+		QUITE_CHECK(tempBytesWritten != -1 || EINTR || EAGAIN || EWOULDBLOCK);
+	}
+
 	QUITE_CHECK(tempBytesWritten != -1);
 
 cleanup:
+	errno = 0;
 	*bytesWritten = tempBytesWritten;
 	return err;
 }
 
-THROWS  err_t create_socket(int domain, int type, int protocal, fd_t *sd)
+THROWS err_t createSocket(int domain, int type, int protocal, fd_t *sd)
 {
 	err_t err = NO_ERRORCODE;
+	errno = 0;
+
 	QUITE_CHECK(sd != NULL);
 	QUITE_CHECK(IS_INVALID_FD(*sd));
-
 	sd->fd = socket(domain, type, protocal);
 	QUITE_CHECK(IS_VALID_FD(*sd));
 
 cleanup:
+	errno = 0;
 	return err;
 }
 
-THROWS  err_t safe_bind(fd_t sd, const struct sockaddr *const addr, socklen_t addrlen)
+THROWS err_t safeBind(fd_t sd, const struct sockaddr *const addr, socklen_t addrlen)
 {
 	err_t err = NO_ERRORCODE;
+	errno = 0;
+
 	QUITE_CHECK(IS_VALID_FD(sd));
 	QUITE_CHECK(addr != NULL);
 
 	CHECK(bind(sd.fd, addr, addrlen) == 0);
+	errno = 0;
 cleanup:
 	return err;
 }
